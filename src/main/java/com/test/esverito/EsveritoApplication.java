8d9fa5d787cade776d398ab8cf45c4c2f10199e1
engineer027 <dp270597dma@gmail.com>
2021-03-08 13:51:31 +0200

    first commit

---
+++ src/main/java/com/test/esverito/EsveritoApplication.java
@@ -0,0 +1,13 @@
+package com.test.esverito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsveritoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsveritoApplication.class, args);
+    }
+
+}
